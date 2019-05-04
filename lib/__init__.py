66552905054543b5fa78dda2a41809bf8df75b5f
KV <kvasudev@ualberta.ca>
2019-05-03 21:42:25 -0700

    Used to import /lib/ files

---
+++ lib/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tuesday, Apr 26, 2019
+@author: kvasudev
+@email: kvasudev@ualberta.ca
+@status: full function with limitations
+"""
+
+# import packages
+import lib.fcnLibrary
+import lib.builder
+import lib.handler
+import lib.button_handler
+import lib.signal
