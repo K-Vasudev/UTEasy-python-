5c5fffba5ad3745bbd7e0e39f6d6ce4a282e7a09
KV <kvasudev@ualberta.ca>
2019-05-11 18:21:16 -0700

    Contains Units tests for UTEasy

---
+++ test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tuesday, Apr 26, 2019
+@author: kvasudev
+@email: kvasudev@ualberta.ca
+@status: full function
+"""
+
+# Import modules
+import lib.fcnLibrary as fcnlib
+import numpy as np
+
+"""
+# Signal.py Tests
+# Test the 'signal.py' UltrasonicSignal class
+"""
+
+
+"""
+# fcnLibrary.py Tests
+# Test the 'fcnLibrary' functions
+"""
+
+# Test for stripNewline
+testStr01 = "String\n"
+testStr02 = fcnlib.stripNewline(testStr01)
+assert testStr02=="String","fcnlib.stripNewline Error"
+
+# Test for createTime
+testTime = fcnlib.createTime(1,5)
+assert (testTime==[0,1,2,3,4]).all(),"fcnlib.createTime Error"
+
+# Test for getTimeStep
+start = 0
+end = 10
+axisLength = 10
+timeStep = fcnlib.getTimeStep(start,end,axisLength)
+assert timeStep==1,"Error in fcnlib.getTimeStep"
+
+# Test mapPeakTime
+time_vect = [0,1,2,3,4,5,6,7,8,9,10]
+idx = [3,6,7]
+peakTime = fcnlib.mapPeakTime(time_vect,idx)
+assert (peakTime==idx).all(),"Error in fcnLibrary mapPeakTime()"
